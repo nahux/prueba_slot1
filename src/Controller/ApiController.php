3be9455dbf5958625bac78ec951067dcdee59cce
Nahuel Aparicio <naleaparicio@gmail.com>
2019-12-26 15:25:06 -0300

    Dependencias para cliente http y prueba

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -4,16 +4,12 @@ namespace App\Controller;
 
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\Controller\FOSRestController;
-use JMS\SerializerBundle\JMSSerializerBundle;
-use Symfony\Component\Config\Definition\Exception\Exception;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Nelmio\ApiDocBundle\Annotation\Model;
 use Swagger\Annotations as SWG;
+use Unirest;
 
 /**
  * Class ApiController
@@ -22,47 +18,69 @@ use Swagger\Annotations as SWG;
  */
 class ApiController extends AbstractFOSRestController
 {
+	private const APP_ID_PARAMETER = "&lang=es&appid=";
+	private $api_url;
+	private $api_secret;
 
-		public function __construct()
-		{
-		}
-    /**
-     * @Rest\Get("/city_weather/", name="city_weather")
-     *
-     * @SWG\Response(
-     *     response=200,
-     *     description="Consulta exitosa"
-     * ),
-     * @SWG\Response(
-     *     response="400",
-     *     description="Consulta mal formada"
-     *  ),
-     * @SWG\Response(
-     *     response="404",
-     *     description="No se ha encontrado la ciudad"
-     *  ),
-     * @SWG\Response(
-     *     response=500,
-     *     description="Ocurrió un error interno"
-     * ),
-     *
-     * @SWG\Parameter(
-     *     name="city_name",
-     *     in="header",
-     *     type="string",
-     *     description="Nombre de la ciudad a buscar el clima",
-     *     schema={
-     *     }
-     * )
-     *
-     * @SWG\Tag(name="Clima")
-		 * 
-		 * @author Nahuel Aparicio
-		 * @param Request request
-		 * @return JsonResponse
-     */
-    public function getCityWeather(Request $request) {
-			return new Response("hola");
-		}
+	public function __construct($api_url, $api_secret)
+	{
+		$this->api_url = $api_url;
+		$this->api_secret = $api_secret;
+	}
 
+	/**
+	 * @author Nahuel Aparicio
+	 * @param String url
+	 * @return Response
+	 */
+	private function callRequest($url_parameters){
+	
+		// $response = $client-
+			
+		// return $response;
+	}
+
+	/**
+	 * @Rest\Get("/city_weather/", name="city_weather")
+	 *
+	 * @SWG\Response(
+	 *     response=200,
+	 *     description="Consulta exitosa"
+	 * ),
+	 * @SWG\Response(
+	 *     response="400",
+	 *     description="Consulta mal formada"
+	 *  ),
+	 * @SWG\Response(
+	 *     response="404",
+	 *     description="No se ha encontrado la ciudad"
+	 *  ),
+	 * @SWG\Response(
+	 *     response=500,
+	 *     description="Ocurrió un error interno"
+	 * ),
+	 *
+	 * @SWG\Parameter(
+	 *     name="city_name",
+	 *     in="header",
+	 *     type="string",
+	 *     description="Nombre de la ciudad a buscar el clima",
+	 *     schema={
+	 *     }
+	 * )
+	 *
+	 * @SWG\Tag(name="Clima")
+	 * 
+	 * @author Nahuel Aparicio
+	 * @param Request request
+	 * @return JsonResponse
+	 */
+	public function getCityWeather(Request $request)
+	{
+		// Seteo la url con la ciudad y el secret de la api de OpenWeather
+		$url_parameters = "?q=La Plata" . self::APP_ID_PARAMETER. $this->api_secret;
+
+		$response = Unirest\Request::get($this->api_url, $url_parameters);
+		return new Response(json_encode($response));
+	}
 }
