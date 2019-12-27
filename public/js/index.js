276a52f8930ca3c082a6a7c5248d3a184a024b6f
Nahuel Aparicio <naleaparicio@gmail.com>
2019-12-27 15:47:49 -0300

    Fixes infos del clima y comentado del codigo js

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,29 @@
 $(document).ready(function () {
 
 	$("#boton-buscar").click(function () {
+		// Muestro spinner cargando y borro el contenido anterior
 		$("#spinner").show();
 		$("#error").hide();
 		$(".container-info-clima").hide();
+
+		// Traigo ciudad del input
 		var ciudad = $("#input-buscar").val();
+		// Llamo por ajax a la api
 		var url = window.location.href + "api/city_weather/?city_name=" + ciudad;
 		var request = $.ajax(url)
 			.done(function(result) {
-				var info_clima = $.parseJSON(result).info_clima;
+				// Muestro la barra separadora y oculto el spinner
 				$("#barra").show();
 				$("#spinner").hide();
-
-				console.log(info_clima.clima);
-				$("#clima").text(info_clima.clima);
-				$("#temp").text(info_clima.temperatura+"°C");
-				$("#viento").text(info_clima.viento+"km");
+				
+				var info_clima = $.parseJSON(result).info_clima;
+				// Seteo los valores y los muestro
+				var titulo_ciudad = ciudad.toLowerCase(); // Ciudad
+				$("#ciudad").text(titulo_ciudad);
+				$("#ciudad").css('text-transform', 'capitalize');
+				$("#clima").text(info_clima.clima); // Clima
+				$("#temp").text(info_clima.temperatura+"°C"); // Temperatura
+				$("#viento").text(info_clima.viento+"km"); // Viento
 				$(".container-info-clima").show();
 			})
 			.fail(function(error) {
@@ -28,9 +36,11 @@ $(document).ready(function () {
 	// Onchange del input del buscador
 	$("#input-buscar").keyup(function (event) {
 		if ($("#input-buscar").val()) {
+			// Si hay texto en el input habilito el boton buscar
 			$("#boton-buscar").removeAttr("disabled");
 			$("#boton-buscar").addClass('boton-buscar-active');
 		} else {
+			// Si NO hay texto en el input deshabilito el boton buscar
 			$("#boton-buscar").attr("disabled", "");
 			$("#boton-buscar").removeClass('boton-buscar-active');
 		}
