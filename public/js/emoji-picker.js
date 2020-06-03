ec81bb4db1b5e578d6495c6a9c35ca985a029234
Nahuel Aparicio <nahuel.aparicio@slot1.io>
2020-06-03 11:52:35 -0300

    Sorted emojis and initial preview div

--- public/js/emoji-picker.js
+++ public/js/emoji-picker.js
@@ -1,15 +1,21 @@
 // URLs
 var emoji_json_url = 'https://cdn.jsdelivr.net/npm/emoji-datasource@5.0.1/emoji.json';
-var sheet_url = 'img/sheet_apple_64.png';
+var sheet_url = 'img/sheet_twitter_64.png';
 var emoji_px_size = 64;
 var sheet_size = 1000;
 var textarea = document.getElementById("input-emoji");
 
-// Get emoji JSON
-var emoji_data = {};
-$.getJSON(emoji_json_url, function(result){
-    emoji_data = result;
-});
+/**
+ * Returns a json stored in a url
+ * @param {string} json_url
+ */
+function getJsonFromUrl(json_url){
+    var Httpreq = new XMLHttpRequest();
+    Httpreq.open("GET",json_url,false);
+    Httpreq.send(null);
+    return Httpreq.responseText;          
+}
+
 var categories_order = {
     'Symbols': 8,
     'Activities': 5,
@@ -36,11 +42,24 @@ function showPicker(button) {
         emoji_container.style.display = 'block';
         return emoji_container;
     }
-    
+    // Get emoji JSON
+    var emoji_data = {};
+    emoji_data = sortByKey(JSON.parse(getJsonFromUrl(emoji_json_url)), 'sort_order');
+        
     // If not setup container in DOM
     emoji_container = createElement("div", "emoji-picker-container");
     emoji_container.id = "emoji-picker-container";
     emoji_container.style.display = 'block';
+
+    // Spinner while loading
+    var emoji_spinner = createElement("div", "spinner");
+    emoji_spinner.id = "emoji-picker-spinner";
+    emoji_spinner.appendChild(createElement("div", "double-bounce1"))
+    emoji_spinner.appendChild(createElement("div", "double-bounce2"))
+    emoji_container.appendChild(emoji_spinner);
+
+    // Pop up container
+    button.parentNode.insertBefore(emoji_container, button);
     
     // Container content
     var emoji_list = createElement("div", "emoji-picker-option-container");
@@ -51,7 +70,7 @@ function showPicker(button) {
     var category_div = setCategoryTitleNode(current_category, emoji_list, category_nodes);
     for (var i = 0; i < emoji_data.length; i++) {
         // Category check
-        if (emoji_data[i].category == 'Skin Tones') continue;
+        if (typeof categories_order[emoji_data[i].category] === 'undefined' ) continue;
         if (current_category != emoji_data[i].category) {
             current_category = emoji_data[i].category;
             category_div = setCategoryTitleNode(current_category, emoji_list, category_nodes);
@@ -69,15 +88,20 @@ function showPicker(button) {
         // Set emoji option button
         var emoji_option = createElement("button", "emoji-picker-option");
         emoji_option.id = emoji.short_name;
+        emoji_option.title = emoji.name;
         emoji_option.style = getCssBackgroundFromSheet(emoji.sheet_x, emoji.sheet_y);
         emoji_option.dataset['unicode'] =  emoji_text;
         emoji_option.onclick = selectEmoji;
+        emoji_option.onmouseover = showEmojiPreview(emoji_option);
+        emoji_option.onmouseout = hideEmojiPreview;
         category_div.appendChild(emoji_option);
     }
+    emoji_spinner.style.display = 'none';
     emoji_container.appendChild(emoji_list);
-
-    // Pop up container
-    button.parentNode.insertBefore(emoji_container, button);
+    // Emoji preview div
+    var emoji_preview = createElement("div", "emoji-picker-emoji-preview");
+    emoji_preview.id = "emoji-picker-emoji-preview";
+    emoji_container.appendChild(emoji_preview);
 }
 
 
@@ -141,6 +165,29 @@ function setContainerToggleListener() {
 }
 
 /**
+ * Show emoji preview
+ */
+function showEmojiPreview(emoji_option) {
+    return function() {
+            var emoji_preview = document.getElementById("emoji-picker-emoji-preview");
+            emoji_preview.style.display = 'block';
+            emoji_preview.innerHTML = emoji_option.title;
+            var emoji_preview_button = emoji_option.cloneNode(true);
+            emoji_preview.appendChild(emoji_preview_button);
+        }
+    }
+/**
+ * Hide emoji preview
+ */
+function hideEmojiPreview() {
+    setTimeout(function(){
+        var emoji_preview = document.getElementById("emoji-picker-emoji-preview");
+        emoji_preview.style.display = 'none';
+        emoji_preview.innerHTML = "";
+    }, 5000);
+}
+
+/**
     * Calculate position in sheet
     * @param {int} x - X position on the sheet grid
     * @param {int} y - Y position on the sheet grid
@@ -167,3 +214,18 @@ function selectEmoji() {
     var emoji_html = twemoji.parse(emoji_unicode, {folder:'svg', ext:'.svg'});
     textarea.innerHTML += emoji_html;
 }
+
+/** 
+ * Helper to sort emoji json by property
+ * @param {Array} array - Emoji data json
+ * @params {string} key - array key to use to sort
+ * @return {Array} emoji_data - Sorted emoji data
+ */
+function sortByKey(array, key) {
+    return array.sort(function(a, b) {
+        var x = a[key]; 
+        var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+}
+    
